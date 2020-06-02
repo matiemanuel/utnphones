0f5942adb52962bc5292d3195691d9294adeea69
Matias Ceraño <matiemanuel@gmail.com>
2020-06-01 21:02:11 -0300

    junit added

--- src/test/java/edu/utn/utnphones/UtnphonesApplicationTests.java
+++ src/test/java/edu/utn/utnphones/UtnphonesApplicationTests.java
@@ -1,23 +1,32 @@
 package edu.utn.utnphones;
 
 import edu.utn.utnphones.controller.CityController;
+import edu.utn.utnphones.controller.UserController;
 import edu.utn.utnphones.model.City;
-import org.junit.jupiter.api.Test;
+import edu.utn.utnphones.model.Province;
+import edu.utn.utnphones.projections.MostCalledProjection;
+import org.junit.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UtnphonesApplicationTests {
 
-    @Autowired
+    @Mock
     CityController controller;
+
+    @Autowired
+    UserController user;
 
     @Test
     void contextLoads() {
@@ -25,9 +34,27 @@ class UtnphonesApplicationTests {
 
     @Test
     public void Testing() {
+
+        List<City> list = new ArrayList<>();
+        list.add(new City(1, "Mar del Plata", "223", new Province(1, "Buenos Aires")));
+        when(controller.getAll(null)).thenReturn(ResponseEntity.ok(list));
+
         ResponseEntity<List<City>> all = controller.getAll(null);
+
         assertEquals(HttpStatus.OK, all.getStatusCode());
         assertNotEquals(0, all.getBody().size());
+    }
+
+    @Test
+    public void mostCalledTest() {
+
+        MostCalledProjection mostCalledFromUser = user.getMostCalledFromUser(1);
+
+        assertEquals("Juan", mostCalledFromUser.getName());
+        assertEquals("Perez", mostCalledFromUser.getLastname());
+        assertEquals("3582545456", mostCalledFromUser.getMostCalled());
+
+
     }
 
 }
